66a74997df0688d34f3b0559ccfc69146c00aef0
rabbitswap <80514740+rabbitswap@users.noreply.github.com>
2021-03-12 15:51:58 +0100

    Update MasterChef.sol

--- contracts/MasterChef.sol
+++ contracts/MasterChef.sol
@@ -26,13 +26,13 @@ contract MasterChef is Ownable, ReentrancyGuard {
         uint256 amount;         // How many LP tokens the user has provided.
         uint256 rewardDebt;     // Reward debt. See explanation below.
         //
-        // We do some fancy math here. Basically, any point in time, the amount of VIRUSs
+        // We do some fancy math here. Basically, any point in time, the amount of CARROTs
         // entitled to a user but is pending to be distributed is:
         //
-        //   pending reward = (user.amount * pool.accVirusPerShare) - user.rewardDebt
+        //   pending reward = (user.amount * pool.accCarrotPerShare) - user.rewardDebt
         //
         // Whenever a user deposits or withdraws LP tokens to a pool. Here's what happens:
-        //   1. The pool's `accVirusPerShare` (and `lastRewardBlock`) gets updated.
+        //   1. The pool's `accCarrotPerShare` (and `lastRewardBlock`) gets updated.
         //   2. User receives the pending reward sent to his/her address.
         //   3. User's `amount` gets updated.
         //   4. User's `rewardDebt` gets updated.
@@ -41,19 +41,19 @@ contract MasterChef is Ownable, ReentrancyGuard {
     // Info of each pool.
     struct PoolInfo {
         IBEP20 lpToken;           // Address of LP token contract.
-        uint256 allocPoint;       // How many allocation points assigned to this pool. VIRUSs to distribute per block.
-        uint256 lastRewardBlock;  // Last block number that VIRUSs distribution occurs.
-        uint256 accVirusPerShare;   // Accumulated VIRUSs per share, times 1e12. See below.
+        uint256 allocPoint;       // How many allocation points assigned to this pool. CARROTs to distribute per block.
+        uint256 lastRewardBlock;  // Last block number that CARROTs distribution occurs.
+        uint256 accCarrotPerShare;   // Accumulated CARROTs per share, times 1e12. See below.
         uint16 depositFeeBP;      // Deposit fee in basis points
     }
 
-    // The VIRUS TOKEN!
-    CarrotToken public virus;
+    // The Carrot TOKEN!
+    CarrotToken public carrot;
     // Dev address.
     address public devaddr;
-    // VIRUS tokens created per block.
-    uint256 public virusPerBlock;
-    // Bonus muliplier for early virus makers.
+    // CARROT tokens created per block.
+    uint256 public carrotPerBlock;
+    // Bonus muliplier for early carrot makers.
     uint256 public constant BONUS_MULTIPLIER = 1;
     // Deposit Fee address
     address public feeAddress;
@@ -64,7 +64,7 @@ contract MasterChef is Ownable, ReentrancyGuard {
     mapping(uint256 => mapping(address => UserInfo)) public userInfo;
     // Total allocation points. Must be the sum of all allocation points in all pools.
     uint256 public totalAllocPoint = 0;
-    // The block number when VIRUS mining starts.
+    // The block number when CARROT mining starts.
     uint256 public startBlock;
 
     event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
@@ -72,19 +72,19 @@ contract MasterChef is Ownable, ReentrancyGuard {
     event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
     event SetFeeAddress(address indexed user, address indexed newAddress);
     event SetDevAddress(address indexed user, address indexed newAddress);
-    event UpdateEmissionRate(address indexed user, uint256 virusPerBlock);
+    event UpdateEmissionRate(address indexed user, uint256 carrotPerBlock);
 
     constructor(
-        CarrotToken _virus,
+        CarrotToken _carrot,
         address _devaddr,
         address _feeAddress,
-        uint256 _virusPerBlock,
+        uint256 _carrotPerBlock,
         uint256 _startBlock
     ) public {
-        virus = _virus;
+        carrot = _carrot;
         devaddr = _devaddr;
         feeAddress = _feeAddress;
-        virusPerBlock = _virusPerBlock;
+        carrotPerBlock = _carrotPerBlock;
         startBlock = _startBlock;
     }
 
@@ -111,12 +111,12 @@ contract MasterChef is Ownable, ReentrancyGuard {
         lpToken : _lpToken,
         allocPoint : _allocPoint,
         lastRewardBlock : lastRewardBlock,
-        accVirusPerShare : 0,
+        accCarrotPerShare : 0,
         depositFeeBP : _depositFeeBP
         }));
     }
 
-    // Update the given pool's VIRUS allocation point and deposit fee. Can only be called by the owner.
+    // Update the given pool's CARROT allocation point and deposit fee. Can only be called by the owner.
     function set(uint256 _pid, uint256 _allocPoint, uint16 _depositFeeBP, bool _withUpdate) public onlyOwner {
         require(_depositFeeBP <= 10000, "set: invalid deposit fee basis points");
         if (_withUpdate) {
@@ -132,18 +132,18 @@ contract MasterChef is Ownable, ReentrancyGuard {
         return _to.sub(_from).mul(BONUS_MULTIPLIER);
     }
 
-    // View function to see pending VIRUSs on frontend.
-    function pendingVirus(uint256 _pid, address _user) external view returns (uint256) {
+    // View function to see pending CARROT on frontend.
+    function pendingCarrot(uint256 _pid, address _user) external view returns (uint256) {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][_user];
-        uint256 accVirusPerShare = pool.accVirusPerShare;
+        uint256 accCarrotPerShare = pool.accCarrotPerShare;
         uint256 lpSupply = pool.lpToken.balanceOf(address(this));
         if (block.number > pool.lastRewardBlock && lpSupply != 0) {
             uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-            uint256 virusReward = multiplier.mul(virusPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-            accVirusPerShare = accVirusPerShare.add(virusReward.mul(1e12).div(lpSupply));
+            uint256 carrotReward = multiplier.mul(carrotPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+            accCarrotPerShare = accCarrotPerShare.add(carrotReward.mul(1e12).div(lpSupply));
         }
-        return user.amount.mul(accVirusPerShare).div(1e12).sub(user.rewardDebt);
+        return user.amount.mul(accCarrotPerShare).div(1e12).sub(user.rewardDebt);
     }
 
     // Update reward variables for all pools. Be careful of gas spending!
@@ -166,22 +166,22 @@ contract MasterChef is Ownable, ReentrancyGuard {
             return;
         }
         uint256 multiplier = getMultiplier(pool.lastRewardBlock, block.number);
-        uint256 virusReward = multiplier.mul(virusPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
-        virus.mint(devaddr, virusReward.div(10));
-        virus.mint(address(this), virusReward);
-        pool.accVirusPerShare = pool.accVirusPerShare.add(virusReward.mul(1e12).div(lpSupply));
+        uint256 carrotReward = multiplier.mul(carrotPerBlock).mul(pool.allocPoint).div(totalAllocPoint);
+        carrot.mint(devaddr, carrotReward.div(10));
+        carrot.mint(address(this), carrotReward);
+        pool.accCarrotPerShare = pool.accCarrotPerShare.add(carrotReward.mul(1e12).div(lpSupply));
         pool.lastRewardBlock = block.number;
     }
 
-    // Deposit LP tokens to MasterChef for VIRUS allocation.
+    // Deposit LP tokens to MasterChef for CARROT allocation.
     function deposit(uint256 _pid, uint256 _amount) public nonReentrant {
         PoolInfo storage pool = poolInfo[_pid];
         UserInfo storage user = userInfo[_pid][msg.sender];
         updatePool(_pid);
         if (user.amount > 0) {
-            uint256 pending = user.amount.mul(pool.accVirusPerShare).div(1e12).sub(user.rewardDebt);
+            uint256 pending = user.amount.mul(pool.accCarrotPerShare).div(1e12).sub(user.rewardDebt);
             if (pending > 0) {
-                safeVirusTransfer(msg.sender, pending);
+                safeCarrotTransfer(msg.sender, pending);
             }
         }
         if (_amount > 0) {
@@ -194,7 +194,7 @@ contract MasterChef is Ownable, ReentrancyGuard {
                 user.amount = user.amount.add(_amount);
             }
         }
-        user.rewardDebt = user.amount.mul(pool.accVirusPerShare).div(1e12);
+        user.rewardDebt = user.amount.mul(pool.accCarrotPerShare).div(1e12);
         emit Deposit(msg.sender, _pid, _amount);
     }
 
@@ -204,15 +204,15 @@ contract MasterChef is Ownable, ReentrancyGuard {
         UserInfo storage user = userInfo[_pid][msg.sender];
         require(user.amount >= _amount, "withdraw: not good");
         updatePool(_pid);
-        uint256 pending = user.amount.mul(pool.accVirusPerShare).div(1e12).sub(user.rewardDebt);
+        uint256 pending = user.amount.mul(pool.accCarrotPerShare).div(1e12).sub(user.rewardDebt);
         if (pending > 0) {
-            safeVirusTransfer(msg.sender, pending);
+            safeCarrotTransfer(msg.sender, pending);
         }
         if (_amount > 0) {
             user.amount = user.amount.sub(_amount);
             pool.lpToken.safeTransfer(address(msg.sender), _amount);
         }
-        user.rewardDebt = user.amount.mul(pool.accVirusPerShare).div(1e12);
+        user.rewardDebt = user.amount.mul(pool.accCarrotPerShare).div(1e12);
         emit Withdraw(msg.sender, _pid, _amount);
     }
 
@@ -227,16 +227,16 @@ contract MasterChef is Ownable, ReentrancyGuard {
         emit EmergencyWithdraw(msg.sender, _pid, amount);
     }
 
-    // Safe virus transfer function, just in case if rounding error causes pool to not have enough VIRUSs.
-    function safeVirusTransfer(address _to, uint256 _amount) internal {
-        uint256 virusBal = virus.balanceOf(address(this));
+    // Safe carrot transfer function, just in case if rounding error causes pool to not have enough CARROT.
+    function safeCarrotTransfer(address _to, uint256 _amount) internal {
+        uint256 carrotBal = carrot.balanceOf(address(this));
         bool transferSuccess = false;
-        if (_amount > virusBal) {
-            transferSuccess = virus.transfer(_to, virusBal);
+        if (_amount > carrotBal) {
+            transferSuccess = carrot.transfer(_to, carrotBal);
         } else {
-            transferSuccess = virus.transfer(_to, _amount);
+            transferSuccess = carrot.transfer(_to, _amount);
         }
-        require(transferSuccess, "safeVirusTransfer: transfer failed");
+        require(transferSuccess, "safeCarrotTransfer: transfer failed");
     }
 
     // Update dev address by the previous dev.
@@ -253,9 +253,9 @@ contract MasterChef is Ownable, ReentrancyGuard {
     }
 
     //Pancake has to add hidden dummy pools inorder to alter the emission, here we make it simple and transparent to all.
-    function updateEmissionRate(uint256 _virusPerBlock) public onlyOwner {
+    function updateEmissionRate(uint256 _carrotPerBlock) public onlyOwner {
         massUpdatePools();
-        virusPerBlock = _virusPerBlock;
-        emit UpdateEmissionRate(msg.sender, _virusPerBlock);
+        carrotPerBlock = _carrotPerBlock;
+        emit UpdateEmissionRate(msg.sender, _carrotPerBlock);
     }
 }
